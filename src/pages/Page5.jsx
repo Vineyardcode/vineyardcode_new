0c8510262fe328dec273f1462c54719c589b94c0
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-05-23 15:19:37 +0200

    master

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -57,7 +57,7 @@ export default function Page5() {
           <iframe
             width="592px"
             height="727px"
-            src="../api/cors-proxy"
+            src="https://heartandball.vercel.app/"
             title="ballandball"
             frameborder="0"
             allowfullscreen
