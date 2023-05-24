2586b0b52d1c612513dfbff76149b3aaff0d4b87
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-05-24 15:28:17 +0200

    master

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -1,22 +1,15 @@
 import * as THREE from "three";
-import React, { Suspense, useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Html, Environment, useGLTF, ContactShadows, OrbitControls, PerspectiveCamera, CameraControls } from '@react-three/drei';
-import { FaGithub, FaCodepen, FaReact, FaJs, SiTypescript, FaPython, VscCode, SiVisualstudiocode, FaGit, FaGithubSquare, GiTBrick, DiGithubAlt, GoGithubAction, DiGithubBadge, RiGithubFill, DiGit, SiRedux, DiHtml5, DiCss3, SiPostgresql, DiGoogleCloudPlatform, DiFirebase, SiThreedotjs } from "react-icons/all"
-import { gsap } from 'gsap';
-import { refs } from "../components/Refs";
-import NavigationButtons from "../components/NavModal";
-import '../styles/page5.css'
-
+import { Html } from '@react-three/drei';
+import '../styles/page5.css';
 
 export default function Page5() {
-
-
   const group = useRef();
   const mesh = useRef();
+  const [iframeReady, setIframeReady] = useState(false);
 
   useFrame((state) => {
-
     const t = state.clock.getElapsedTime();
 
     group.current.rotation.x = THREE.MathUtils.lerp(
@@ -24,53 +17,44 @@ export default function Page5() {
       Math.cos(t / 2) / 1000 + 0.1,
       0.01
     );
-    // group.current.rotation.y = THREE.MathUtils.lerp(
-    //   group.current.rotation.y,
-    //   Math.sin(t / 4) / 100,
-    //   0.1
-    // );
-    // group.current.rotation.z = THREE.MathUtils.lerp(
-    //   group.current.rotation.z,
-    //   Math.sin(t / 8) / 100,
-    //   0.1
-    // );
   });
 
+  useEffect(() => {
+    if (iframeReady) {
+      // Reload the iframe once the scene is ready
+      const iframe = document.getElementById("myIframe");
+      iframe.contentWindow.location.reload();
+    }
+  }, [iframeReady]);
+
+  const handleIframeLoad = () => {
+    setIframeReady(true);
+  };
 
   return (
-
-    <group ref={group} dispose={null} >
-
-      <mesh ref={mesh} >
-      <boxGeometry args={[14.8, 1, 18.2]} />
-      <meshStandardMaterial 
-        color={0xffffff}
-      />
-          <Html 
-            className="page5"  
-            transform occlude="blending" 
-            rotation={[-Math.PI/2,0,0]} 
-            position={[0,0.52,0]}
-            style={{ width: "592px", height: "727px" }}
-          >
-           
+    <group ref={group} dispose={null}>
+      <mesh ref={mesh}>
+        <boxGeometry args={[14.8, 1, 18.2]} />
+        <meshStandardMaterial color={0xffffff} />
+        <Html
+          className="page5"
+          transform occlude="blending"
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, 0.52, 0]}
+          style={{ width: "592px", height: "727px" }}
+        >
           <iframe
+            id="myIframe"
             width="592px"
             height="727px"
             src="https://heartandball.vercel.app/"
             title="ball"
+            frameBorder={0}
             allowFullScreen
-
-            >
-
-          </iframe>
-
-
-          </Html>
-          
+            onLoad={handleIframeLoad}
+          ></iframe>
+        </Html>
       </mesh>
     </group>
- 
   );
 }
-
